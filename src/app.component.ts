6afa18a6dbd9268570a339a996492650aaa89a80
Keyur Mistry <keyurmistry19@gmail.com>
2025-07-01 19:33:55 +0530

    edit env folders commit.

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,10 +3,10 @@ import { RouterModule } from '@angular/router';
 import { MessageService } from 'primeng/api';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`,
-    providers:[MessageService]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule],
+  template: `<router-outlet></router-outlet>`,
+  providers: [MessageService],
 })
 export class AppComponent {}
--- src/app/pages/service/user.service.ts
+++ src/app/pages/service/user.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { API_BASE } from '../../../app.constants';
-
+import { environment } from '../../../environments/environment';
 
 interface LoginResponse {
   token: string;
@@ -11,24 +10,18 @@ interface LoginResponse {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  private readonly API_URL = `${API_BASE}/users`
+  private readonly API_URL = `${environment.apiBaseUrl}/users`;
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http
-      .get<any>(`${this.API_URL}/all`)
+    return this.http.get<any>(`${this.API_URL}/all`);
   }
 
-  getById(id:number):Observable<any>{
-    return this.http.get<any>(`${this.API_URL}/${id}`)
+  getById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/${id}`);
   }
-  
-
-
-  
 }
